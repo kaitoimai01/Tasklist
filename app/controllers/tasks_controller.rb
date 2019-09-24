52ae07a4c951d3022ac40879198fe16428580e56
今井 海斗 <1111961@kimai.local>
2019-09-24 16:41:47 +0900

    add create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,14 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to root_path, notice: 'タスクが追加されました。'
+    else
+      flash[:danger] = 'タスクが追加できません。'
+      render :new
+    end
   end
 
   def edit
@@ -17,5 +25,10 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  # Strong paramater
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
