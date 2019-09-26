15098177e2dd73cc540ddf64fd554d359d04448f
今井 海斗 <1111961@kimai.local>
2019-09-26 17:53:47 +0900

    add validates

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
     default_scope -> { order(created_at: :desc) }
+
+    validates :title, presence: true, length: { maximum: 50 }
+    validates :content, presence: true, length: { maximum: 255 }
 end
