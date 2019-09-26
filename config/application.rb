418c64b131bd5c3d528830cee5edb30e00fb2cd8
今井 海斗 <1111961@kimai.local>
2019-09-26 18:36:43 +0900

    fix ja.yml

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module App
     # the framework and any gems in your application.
 
     # Don't generate system test files.
+    config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     config.time_zone = 'Asia/Tokyo'
     config.generators.system_tests = nil
