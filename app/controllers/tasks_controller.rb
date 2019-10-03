c32fae5207136c06269ac8a8d2432acaf1962066
今井 海斗 <1111961@kimai.local>
2019-10-03 13:10:48 +0900

    add deadline

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,6 +45,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :deadline)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
-    default_scope -> { order(created_at: :desc) }
+    default_scope -> { order(deadline: :asc) }
 
     validates :title, presence: true, length: { maximum: 50 }
     validates :content, presence: true, length: { maximum: 255 }
+    validates :deadline, presence: true
 end
