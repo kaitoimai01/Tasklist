ba1eafe0a34bf456ab5959b0e0071312a8ccd0db
今井 海斗 <1111961@kimai.local>
2019-10-07 16:59:12 +0900

    add priority

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,6 +50,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :content, :deadline, :status)
+    params.require(:task).permit(:title, :content, :deadline, :status, :priority)
   end
 end
