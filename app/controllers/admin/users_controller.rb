94e0b44100ac89d7d33da07d712ea46428193b7c
kaitoimai01 <jinjingh424@gmail.com>
2020-04-08 20:43:09 +0900

    ユーザー管理機能の作成（既にあったユーザー登録機能から変更）

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,52 @@
 class Admin::UsersController < ApplicationController
+  #TODO:共通箇所まとめる
+
   def index
+    @users = User.all.page(params[:page])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:succeess] = 'ユーザーを登録しました。'
+      # admin/users/showに遷移
+      redirect_to admin_user_path(@user)
+    else
+      flash.now[:danger] = 'ユーザーの登録に失敗しました。'
+      render :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:success] = 'ユーザーを更新しました。'
+      redirect_to admin_user_path(@user)
+    else
+      flash.now[:danger] = 'ユーザーが更新できませんでした。'
+      render :new
+    end
   end
 
   def destroy
+    @uer = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
 Rails.application.routes.draw do
+  root 'admin/users#new'
+
   namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/new'
-    get 'users/edit'
-    get 'users/destroy'
+    resources :users
   end
-  # root 'tasks#index'
-  root 'toppages#index'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
-  get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
   
   resources :tasks
 end
