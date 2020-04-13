2eda1be46d905f5f2079232ddbdf51bd00f446fc
kaitoimai01 <jinjingh424@gmail.com>
2020-04-13 16:38:58 +0900

    ActiveRecord::HasManyThroughorderErrorの解消

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
-  has_many :labels, through: :task_labels
   has_many :task_labels, dependent: :destroy
+  has_many :labels, through: :task_labels
 
 
   default_scope -> { order(priority: :desc) }
